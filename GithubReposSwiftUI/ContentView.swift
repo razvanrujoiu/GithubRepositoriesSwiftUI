85a34bc0c2bdd8e10bc640ca392ca885c35cd1df
Razvan Rujoiu <rjrazvan@icloud.com>
2020-11-23 01:22:18 +0200

    Initial Commit

---
+++ GithubReposSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubReposSwiftUI
+//
+//  Created by Razvan Rujoiu on 23.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubReposSwiftUI/GithubReposSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubReposSwiftUIApp.swift
+//  GithubReposSwiftUI
+//
+//  Created by Razvan Rujoiu on 23.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct GithubReposSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
